95ef5456d26b385d7f886d0d808a548ba588af62
Celine <cc972@cornell.edu>
2018-09-24 21:12:48 -0400

    Game instructions draft, disable button yet to fix.

--- game_instructions.php
+++ game_instructions.php
@@ -14,43 +14,77 @@
 
   <!-- Adds the carousel to this file -->
   <title>Social Dynamics Lab-Policy Lab Pilot Testing</title>
+
+<style>
+</style>
 </head>
-  <body>
- 	 <div class="header" id="myHeader">
-
-  		<h2>Instructions</h2>
-	</div>
-
+<body>
+    <div class="header" id="myHeader">
+        <h2>Instructions</h2>
+    </div>
 
     <div class="wrapper" id="consent">
        <form action="political_id.php" method="post">
-       <p><input type="checkbox" id="myCheck" onclick="javascript:enableButton();" >
-         I have carefully read the instructions and am ready to continue with the test.<br></p>
+           <p><input type="checkbox" class="item">
+           You will see 15 statements about various political and cultural
+           beliefs (plus a practice question that does not count).</p>
+           <br/>
 
+           <p>
+           For each statement:<br/>
 
-     <input type = "submit" class="button" id = "myButton" value = "Continue" onclick="javascript:enableButton();" disabled>
+           <input type="checkbox" class="item">
+           You will see the views of previous participants from each political
+           party.
+           <br/>
 
-     </form>
+           <input type="checkbox" class="item">
+           We will ask you to predict the reason most [D/R] players will give
+           for why your party agrees or disagrees: ideology, history, or
+           popularity?
+           <br/>
 
+           <input type="checkbox" class="item">
+           At the end of the game, the player with the most accurate predictions
+           will win $100, divided equally in case of ties.
+           </p>
+           <br/>
 
+           <p><input type="checkbox" id="myCheck" class="item" onclick="enableButton();">
+               I have carefully read the instructions and am ready to continue with the test.<br>
+           </p>
+
+           <input type="submit" class="button" id="submitButton" value="Continue" disabled>
+        </form>
     </div>
-
-  </body>
+</body>
 
 <script type="text/javascript">
-$(document).onload(() => {
-    enableButton();
+let unselectedItems;
+
+$(document).ready(() => {
+    unselectedItems = $('.item').size();
+    console.log('Number of required items: ', unselectedItems);
+    // toggleButton(false);
 });
 
-// Function for "clicking checkbox to enable button"
-function enableButton() {
-	if(document.getElementById('myCheck').checked){
-		document.getElementById('myButton').disabled='';
-	} else {
-		document.getElementById('myButton').disabled='true';
-	}
+$('.item').click((e) => {
+    if (e.target.checked) {
+        unselectedItems--;
+    } else {
+        unselectedItems++;
+    }
+
+    toggleButton(unselectedItems !== 0);
+});
+
+/**
+ * Whether to enable button.
+ * @param {boolean} enable
+ */
+function toggleButton(enable) {
+	$('#submitButton').prop('disabled', !enable);
 }
 </script>
-  </html>
 
 </html>
--- instruction2.php
+++ instruction2.php
@@ -32,23 +32,6 @@
 
 
     <div class="wrapper" id="consent">
-        <p>
-         Dear Participant,
-         <p>
-         Please read the following instructions carefully before you begin.
-         </p>
-        <p>
-          The Political Prediction Game tests how well contestants can predict the views of Democrats and Republicans on questions about which the parties have not yet taken a stance. At the end of the study, the contestant with the most accurate predictions will win a $100 prize (to be divided equally in case of ties).
-          Accordingly, we will show you 16 statements and you have to predict why a party is more likely to agree.
-          We will also ask you whether you agree or disagree with the statement.
-        </p>
-
-        <p>
-          Please note that once you submit your answers and move onto the next item, you will not be able to go back and change your responses. Please do not press the back button on your browser, or you will risk causing your session to end prematurely, which will prevent you from being paid for completing the test.
-        </p>
-        <br><br>
-
-
        <form action="political_id.php" method="post">
        <p><input type="checkbox" id="myCheck" onclick="javascript:enableButton();" >
          I have carefully read the instructions and am ready to continue with the test.<br></p>
@@ -57,10 +40,7 @@
      <input type = "submit" class="button" id = "myButton" value = "Continue" onclick="javascript:enableButton();" disabled>
 
      </form>
-
-
     </div>
-
   </body>
 
 <script type="text/javascript">
@@ -73,6 +53,4 @@ function enableButton() {
 	}
 }
 </script>
-  </html>
-
 </html>
