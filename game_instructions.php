c1e26b0b5c2c7bd35287bc24a1e0549c5fcf47c6
Celine <cc972@cornell.edu>
2018-09-24 20:29:59 -0400

    Modifying instructions

--- game_instructions.php
+++ game_instructions.php
@@ -8,23 +8,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" type="text/css" href="styles/style2.css" media="all" />
 
-
   <script src="//code.jquery.com/jquery-1.12.1.min.js"></script>
   <script src="script/jquery.backDetect.js"></script>
   <script src="script/back_button.js"></script>
 
-
-
-
   <!-- Adds the carousel to this file -->
   <title>Social Dynamics Lab-Policy Lab Pilot Testing</title>
-  </head>
-
-
-  <body onload="enableButton();">
-
-
-    <!-- header -->
+</head>
+  <body>
  	 <div class="header" id="myHeader">
 
   		<h2>Instructions</h2>
@@ -32,21 +23,6 @@
 
 
     <div class="wrapper" id="consent">
-        <p>
-         Dear Participant,
-         <p>
-         Please read the following instructions carefully before you begin.
-         </p>
-        <p>
-          The Political Prediction Game tests how well contestants can predict the views of Democrats and Republicans on questions about which the parties have not yet taken a stance. At the end of the study, the contestant with the most accurate predictions will win a $100 prize (to be divided equally in case of ties). Accordingly, we will show you 22 statements and you have to predict which party is more likely to agree. We will also ask you whether you agree or disagree with the statement. You will also see how other participants so far have responded to the question.
-        </p>
-
-        <p>
-          Please note that once you submit your answers and move onto the next item, you will not be able to go back and change your responses. Please do not press the back button on your browser, or you will risk causing your session to end prematurely, which will prevent you from being paid for completing the test.
-        </p>
-        <br><br>
-
-
        <form action="political_id.php" method="post">
        <p><input type="checkbox" id="myCheck" onclick="javascript:enableButton();" >
          I have carefully read the instructions and am ready to continue with the test.<br></p>
@@ -62,6 +38,10 @@
   </body>
 
 <script type="text/javascript">
+$(document).onload(() => {
+    enableButton();
+});
+
 // Function for "clicking checkbox to enable button"
 function enableButton() {
 	if(document.getElementById('myCheck').checked){
